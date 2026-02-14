689799307eb148889df546e99a08eecd54a416e4
varman897 <varmansivarooban2015@gmail.com>
2026-02-13 19:16:45 -0800

    evenmorepython

---
+++ somemorepython.py
@@ -0,0 +1,23 @@
+a=10
+b=12
+c=12
+print(a!=b)
+print(a!=c)
+
+a="python" 
+b="coding"
+
+if a !=b:
+    print(a,'and',b,'are not equal')
+
+a=4
+b=5
+
+if(a==1)!=(b==5):
+ print('Hello')
+
+a=int(input("Enter a number:"))
+
+if a%2 !=0:
+ print(a,"Is not an even number.")
+
